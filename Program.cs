ca3b32cf2b6f5a1cd4b54b433d63ae185a9a7d33
theferhadd <farhad.isgandar@gmail.com>
2020-05-20 12:04:04 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication20
+{
+     class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Xahis olunur novbeti ucun enter basin !");
+            int duz, sehv;
+            duz = 0;
+            sehv = 0;
+            
+            string s1;
+            Console.WriteLine("Sual 1- Python necenci ilde yaradilmishdir ?");
+            s1 = Convert.ToString(Console.ReadLine());
+            string c1;
+            Console.WriteLine(" A)1991\n B)1923\n C)1937\n D)2000\n Cavabiniz: ");
+            
+            c1 = Console.ReadLine();
+            if (c1 == "A" || c1 == "a" )
+            {
+                duz = duz + 1;
+                Console.WriteLine("Duz tapdiniz !");
+                Console.ReadLine();
+            }
+            else
+            {
+                sehv = sehv + 1;
+                Console.WriteLine("Sehv cavab !");
+                Console.ReadLine();
+            }
+            
+            string s2;
+            Console.WriteLine("Sual 2- Ilham Eliyev necenci ilde anadan olmushdur ?");
+            s2 = Convert.ToString(Console.ReadLine());
+
+            string c2;
+            Console.WriteLine(" A)1961\n B)1971\n C)1981\n D)1964\n Cavabiniz: ");
+            
+            c2 = Console.ReadLine();
+            if (c2 == "A" || c2 == "a")
+            {
+                duz++;
+                Console.WriteLine("Duz tapdiniz !");
+                Console.ReadLine();
+            }
+            else
+            {
+                sehv = sehv + 1;
+                Console.WriteLine("Sehv cavab !");
+                Console.ReadLine();
+            }
+            
+            
+            string s3;
+            Console.WriteLine("Sual 3-  Django-nu variantlardan hansina aid etmek olar ?");
+            s3 = Convert.ToString(Console.ReadLine());
+
+            string c3;
+            Console.WriteLine(" A)C#\n B)Python\n C)PHP\n D)C++\n Cavabiniz: ");
+            c3 = Console.ReadLine();
+            if (c3 == "B" || c3 == "b")
+            {
+                duz++;
+                Console.WriteLine("Duz tapdiniz !");
+                Console.ReadLine();
+            }
+            else
+            {
+                sehv = sehv + 1;
+                Console.WriteLine("Sehv cavab !");
+                Console.ReadLine();
+            }
+            
+            string s4;
+            Console.WriteLine("Sual 4-  Codelgniter hansi programlasdirma dilinin framework'udur ?");
+            s4 = Convert.ToString(Console.ReadLine());
+
+            string c4;
+            Console.WriteLine(" A)Python\n B)Java\n C)PHP\n D)C#\n Cavabiniz: ");
+            
+            c4 = Console.ReadLine();
+            if (c4 == "C" || c4 == "c")
+            {
+                duz++;
+                Console.WriteLine("Duz tapdiniz !");
+                Console.ReadLine();
+            }
+            else
+            {
+                sehv = sehv + 1;
+                Console.WriteLine("Sehv cavab !");
+                Console.ReadLine();
+            }
+            
+          
+            string s5;
+            Console.WriteLine("Sual 5-  Linux emeliyyet sisteminin ilk buraxilis ili necedir ?");
+            s5 = Convert.ToString(Console.ReadLine());
+
+            string c5;
+            Console.WriteLine(" A)1991\n B)1981\n C)1971\n D)1955\n Cavabiniz: ");
+            
+            c5 = Console.ReadLine();
+            if (c5 == "A" || c5 == "a")
+            {
+                duz++;
+                Console.WriteLine("Duz tapdiniz !");
+                Console.ReadLine();
+            }
+            else
+            {
+                sehv = sehv + 1;
+                Console.WriteLine("Sehv cavab !");
+                Console.ReadLine();
+            }
+            
+            string s6;
+            Console.WriteLine("Sual 6-  Dunyanin en boyuk web site-i hansidir ?");
+            s6 = Convert.ToString(Console.ReadLine());
+
+            string c6;
+            Console.WriteLine(" A)Google\n B)Wikipedia\n C)Microsoft\n D)BBC\n Cavabiniz: ");
+            
+            c6 = Console.ReadLine();
+            if (c6 == "A" || c6 == "a")
+            {
+                duz++;
+                Console.WriteLine("Duz tapdiniz !");
+                Console.ReadLine();
+            }
+            else
+            {
+                sehv = sehv + 1;
+                Console.WriteLine("Sehv cavab !");
+                Console.ReadLine();
+            }
+            
+            string s7;
+            Console.WriteLine("Sual 7-  En suretli programlasdirma dili hansidir ?");
+            s7 = Convert.ToString(Console.ReadLine());
+
+            string c7;
+            Console.WriteLine(" A)Python\n B)C#\n C)PHP\n D)C, C++\n Cavabiniz: ");
+            
+            c7 = Console.ReadLine();
+            if (c7 == "D" || c7 == "d")
+            {
+                duz++;
+                Console.WriteLine("Duz tapdiniz !");
+                Console.ReadLine();
+            }
+            else
+            {
+                sehv = sehv + 1;
+                Console.WriteLine("Sehv cavab !");
+                Console.ReadLine();
+            }
+            
+            string s8;
+            Console.WriteLine("Sual 8-  Facebook-un ilk adi ne idi ?");
+            s8 = Convert.ToString(Console.ReadLine());
+
+            string c8;
+            Console.WriteLine(" A)Facebook\n B)FaceTable\n C)FaceNote\n D)The Facebook\n Cavabiniz: ");
+            
+            c8 = Console.ReadLine();
+            if (c8 == "D" || c8 == "d")
+            {
+                duz++;
+                Console.WriteLine("Duz tapdiniz !");
+                Console.ReadLine();
+            }
+            else
+            {
+                sehv = sehv + 1;
+                Console.WriteLine("Sehv cavab !");
+                Console.ReadLine();
+            }
+            
+            string s9;
+            Console.WriteLine("Sual 9-  Hansi emeliyyat sisteminin menbe kodlari aciqdir ?");
+            s9 = Convert.ToString(Console.ReadLine());
+
+            string c9;
+            Console.WriteLine(" A)Windows7\n B)Windows8\n C)Windows10\n D)Ubuntu\n Cavabiniz: ");
+            
+            c9 = Console.ReadLine();
+            if (c9 == "D" || c9 == "d")
+            {
+                duz++;
+                Console.WriteLine("Duz tapdiniz !");
+                Console.ReadLine();
+            }
+            else
+            {
+                sehv = sehv + 1;
+                Console.WriteLine("Sehv cavab !");
+                Console.ReadLine();
+            }
+            
+            string s10;
+            Console.WriteLine("Sual 10-  Web site'lar en cox hansi programlasdirma dili ile yazilib ?");
+            s10 = Convert.ToString(Console.ReadLine());
+
+            string c10;
+            Console.WriteLine(" A)Python\n B)C#\n C)PHP\n D)C, C++\\n Cavabiniz: ");
+            
+            c10 = Console.ReadLine();
+            if (c10 == "C" || c10 == "c")
+            {
+                duz++;
+                Console.WriteLine("Duz tapdiniz !");
+                Console.ReadLine();
+            }
+            else
+            {
+                sehv = sehv + 1;
+                Console.WriteLine("Sehv cavab !");
+                Console.ReadLine();
+            }
+            
+            Console.WriteLine("Neticeni gor");
+            if (sehv == 3 || sehv > 3)
+            {
+                Console.WriteLine("Meglub oldunuz");
+                Console.ReadLine();
+            }
+            else if (sehv == 0)
+            {
+                Console.WriteLine("Qalib geldiniz !");
+                Console.ReadLine();
+            }
+            
+            Console.Read();
+            
+        }
+    }
+}
